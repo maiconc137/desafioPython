7b93d90a26f37d2fb32523be4bf1413bbce2e6ba
Tabuada <qumaicon@gmail.com>
2023-04-17 18:53:11 -0300

    resolução ex04 iniciante

--- iniciante/ex004.py
+++ iniciante/ex004.py
@@ -4,3 +4,9 @@ imprima a soma desses números.
 """
 
 # Solução proposta:
+
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+soma = num1 + num2
+
+print('{} + {} = {}'.format(num1, num2, soma))
