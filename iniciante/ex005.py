19d79579ed132cc3d8743556df1b5f4d4608993d
Tabuada <qumaicon@gmail.com>
2023-04-17 21:32:54 -0300

    resolução ex05 iniciante

--- iniciante/ex005.py
+++ iniciante/ex005.py
@@ -4,3 +4,9 @@ imprima o produto desses números.
 """
 
 # Solução proposta:
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+multi = lambda x,y: x * y
+res = multi(num1, num2)
+
+print('Resultado do produto destes número é: {}'.format(res))
