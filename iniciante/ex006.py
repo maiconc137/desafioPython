dff66571d72b1f28b514aafc0b309a6348e8be77
Tabuada <qumaicon@gmail.com>
2023-04-18 11:32:35 -0300

    resolução ex06 iniciante

--- iniciante/ex006.py
+++ iniciante/ex006.py
@@ -4,3 +4,11 @@ imprima se esse número é par ou ímpar.
 """
 
 # Solução proposta:
+
+def par_impar(x):
+    if (x % 2 == 0):
+        return '{} é par.'.format(x)
+    else:
+        return '{} é ímpar.'.format(x)
+res = par_impar(int(input('Digite um número: ')))
+print(res)
