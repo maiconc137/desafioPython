2ce86327e2b604fc7e211a6dc8c8dd389445fdd1
Tabuada <qumaicon@gmail.com>
2023-04-18 13:49:23 -0300

    resolução ex07 iniciante

--- iniciante/ex007.py
+++ iniciante/ex007.py
@@ -4,3 +4,10 @@ imprima se o usuário é maior ou menor de idade.
 """
 
 # Solução proposta:
+
+idade = int(input('Qaul a sua idade? '))
+
+if (idade < 18):
+    print('Você é menor de idade')
+else:
+    print( 'Você é maior de idade')
