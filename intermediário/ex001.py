34803d86aabe611457f1ef1bc5f65a66f534e690
Tabuada <qumaicon@gmail.com>
2023-04-18 22:25:42 -0300

    resolução ex01 intermediário

--- intermediário/ex001.py
+++ intermediário/ex001.py
@@ -4,3 +4,27 @@ números pares.
 """
 
 # Solução proposta:
+
+# lista proposta
+lista = [1,2,3,4,5,6,7,8,9]
+# lista que recebera os valores pares
+lista_par = []
+
+# função que verifica qual número da lista é par
+def num_par(lista_num):
+    for i in lista_num:
+        if (i % 2 == 0):
+            lista_par.append(i)
+    return lista_par
+
+# função que calcula a média dos números pares
+def media_par(x):
+    tam = len(x)
+    soma = 0
+    for i in x:
+        soma += i
+    return soma / tam
+
+# guardando o resultado em uma variável e imprimindo na tela o resultado
+resultado = media_par(num_par(lista))
+print(resultado)
