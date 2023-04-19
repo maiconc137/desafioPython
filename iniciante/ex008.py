b6006f7d56803ad55dd78f95efb4bb1114ca07cf
Tabuada <qumaicon@gmail.com>
2023-04-18 22:06:11 -0300

    resolução ex07 iniciante

--- iniciante/ex008.py
+++ iniciante/ex008.py
@@ -4,3 +4,15 @@ imprima se esse número é positivo, negativo ou zero.
 """
 
 # Solução proposta:
+
+def sinal(x):
+    if not x:
+        return 'Número é zero'
+    elif (x < 0):
+        return 'Número negativo'
+    else:
+        return 'Número positivo'
+
+
+num = int(input('Digite um número: '))
+print(sinal(num))
