cf2279bffb855b2e1f89ab60d969f80a11021938
Tabuada <qumaicon@gmail.com>
2023-04-26 23:13:19 -0300

    resolução ex10 iniciante

--- iniciante/ex010.py
+++ iniciante/ex010.py
@@ -4,3 +4,9 @@ imprima quantas letras essa frase possui.
 """
 
 # Solução proposta:
+
+letras = lambda x: len(x)
+
+frase = input('Digite uma frase')
+qtd_letras = letras(frase)
+print('Quantidade de letra da frase digitada é: {}'.format(qtd_letras))
