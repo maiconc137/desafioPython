5f984d939f24df3e8387469e7ea0c52ffee240a4
Tabuada <qumaicon@gmail.com>
2023-04-28 20:13:12 -0300

    resolução ex14 iniciante

--- iniciante/ex014.py
+++ iniciante/ex014.py
@@ -4,3 +4,16 @@ imprima os números pares de 0 até esse número.
 """
 
 # Solução proposta:
+
+def num_par(x):
+    par = []
+    for i in range(x):
+        if i % 2 == 0:
+            par.append(i)
+        else:
+            continue
+    return par
+
+numeros = int(input('Digite um número inteiro positivo: '))
+numeros_pares = num_par(numeros)
+print(numeros_pares)
