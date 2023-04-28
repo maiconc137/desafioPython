4034cf5e418b84c5ff7af5424c5c7a9b9ba5fbca
Tabuada <qumaicon@gmail.com>
2023-04-27 22:20:28 -0300

    resolução ex12 iniciante

--- iniciante/ex012.py
+++ iniciante/ex012.py
@@ -1,6 +1,16 @@
 """
 Escreva um programa que peça ao usuário para digitar seu nome e, em seguida, 
-imprima seu nome ao contrário.
+imprima seu nome ao contrário (de forma diferente do exercício anterior).
 """
 
 # Solução proposta:
+
+def inverte_palavra(palavra):
+    palavra_invertida = ''
+    for letra in palavra:
+        palavra_invertida = letra + palavra_invertida
+    return palavra_invertida
+
+nome = input('Digite seu nome: ')
+nome_invertido = inverte_palavra(nome)
+print(nome_invertido)
