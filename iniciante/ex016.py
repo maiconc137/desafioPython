cb02a95c4360b57745af79d930c70163ca785e5a
Tabuada <qumaicon@gmail.com>
2023-04-29 22:46:57 -0300

    resolução ex16 iniciante

--- iniciante/ex016.py
+++ iniciante/ex016.py
@@ -5,3 +5,17 @@ os múltiplos de 5 por "Buzz".
 """
 
 # Solução proposta:
+
+def fizz_buzz(x):
+    lista = []
+    for i in range(1, x):
+        if i % 3 == 0:
+            lista.append('Fizz')
+        elif i % 5 == 0:
+            lista.append('Buzz')
+        else:
+            lista.append(i)
+    return lista
+
+num = int(input('Digite um número: '))
+nova_lista = fizz_buzz(num)
