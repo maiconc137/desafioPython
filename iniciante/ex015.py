094cc46a002892aed7fbf05ee6c5b6ef72959c5c
Tabuada <qumaicon@gmail.com>
2023-04-29 22:30:40 -0300

    resolução ex15 iniciante

--- iniciante/ex015.py
+++ iniciante/ex015.py
@@ -4,3 +4,16 @@ imprima os números ímpares de 0 até esse número.
 """
 
 # Solução proposta:
+
+def num_impar(x):
+    impar = []
+    for i in range(x):
+        if i % 2 != 0:
+            impar.append(i)
+        else:
+            continue
+    return impar
+
+num = int(input('Digite um número: '))
+impar = num_impar(num)
+print(impar)
