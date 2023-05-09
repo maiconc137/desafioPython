b27a6b2783b9aa65bfd04f3f00a05da3a14613a8
Tabuada <qumaicon@gmail.com>
2023-05-08 22:25:10 -0300

    resolução ex18 iniciante

--- iniciante/ex018.py
+++ iniciante/ex018.py
@@ -4,3 +4,16 @@ imprima o fatorial desse número.
 """
 
 # Solução proposta:
+
+def fatorial(num):
+  if num == 0 or num == 1:
+    return 1
+  else:
+    fat = 1
+    for i in range(2, num + 1):
+      fat *= i
+    return fat
+
+numero = int(input("Digite um número: "))
+resultado = fatorial(numero)
+print("O fatorial de", numero, "é", resultado)
