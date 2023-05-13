cd13a2b528d5913471da39c2cd1749b02fc9b532
oliveski <oliveski.mesquita@gmail.com>
2023-05-13 01:46:55 -0300

    Resolução do intermediario/ex003

--- intermediário/ex003.py
+++ intermediário/ex003.py
@@ -4,3 +4,20 @@ lista.
 """
 
 # Solução proposta:
+
+def segundo_maior_numero(lista: list) -> float:
+    lista.sort(reverse=True)
+    return lista[1]
+
+exemplo = [
+        1,
+        2,
+        3,
+        3.1,
+        3.14159,
+        -100,
+        0
+    ]
+
+print("O segundo maior número da lista é")
+print(segundo_maior_numero(exemplo))
