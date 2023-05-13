39ff56845b174925f265a8ae183b9f7cb03ecc4f
oliveski <oliveski.mesquita@gmail.com>
2023-05-12 23:45:01 -0300

    Resolução do intermediário/ex002

--- intermediário/ex002.py
+++ intermediário/ex002.py
@@ -3,3 +3,25 @@ Crie uma função que receba uma lista de strings e retorne a string mais longa 
 """
 
 # Solução proposta:
+
+def string_mais_longa(lista: list) -> str:
+    maior_comprimento = -1
+    for i in range(len(lista)):
+        comprimento_string = len(lista[i])
+        if comprimento_string > maior_comprimento:
+            maior_comprimento = comprimento_string
+            maior_string = i
+
+    return lista[maior_string]
+
+
+exemplo = [
+        "teste",
+        "teste1",
+        "teste2",
+        "teste_versão_final",
+        "teste_final_final"
+    ]
+
+print("A string mais longa da lista é")
+print(string_mais_longa(exemplo))
